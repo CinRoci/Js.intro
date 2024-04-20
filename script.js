2cbf018ef9d25aa312c8b92cf048c2e20bc6b80c
CinRoci <166564903+CinRoci@users.noreply.github.com>
2024-04-20 07:09:23 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+/*let num1 = 10;
+let num2 = 2;
+let num3 = 1; 
+
+let mayor = num1 > num2 ? num1 : num2;*/
+
+var x = 10;
+var y = 5;
+var z = 50;
+
+if (x > y) {
+  console.log("x es mayor que y");
+} else if (y > x) {
+  console.log("y es mayor a x");
+} else {
+  console.log("z es mayor");
+}
